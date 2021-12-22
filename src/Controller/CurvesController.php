ec921513e14b69fbe5622fd1d7b8602e9f0eb6df
Slloth <fiquet.noah@gmail.com>
2021-12-22 11:24:42 +0100

    homeController => CurvesController

--- src/Controller/CurvesController.php
+++ src/Controller/CurvesController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class CurvesController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/curves', name: 'curves')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('curves/index.html.twig', [
+            'controller_name' => 'CurvesController',
         ]);
     }
 }
